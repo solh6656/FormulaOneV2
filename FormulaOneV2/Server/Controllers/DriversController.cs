2063a9935349edd3fbcd9bbb9038c4d96c488c02
user <user@WINDOWS-PF2L7HE>
2025-02-05 03:19:11 +0500

    DriversController

--- FormulaOneV2/Server/Controllers/DriversController.cs
+++ FormulaOneV2/Server/Controllers/DriversController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FormulaOneV2.Server.Data;
+using FormulaOneV2.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FormulaOneV2.Server.Controllers
 {
@@ -6,5 +9,69 @@ namespace FormulaOneV2.Server.Controllers
     [Route("api/[controller]")]
     public class DriversController : ControllerBase
     {
+        private readonly ApiDbContext _context;
+        public DriversController(ApiDbContext apiDbContext)
+        {
+            _context = apiDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Driver>>> GetDrivers()
+        {
+            var drivers = await _context.Drives.ToListAsync();
+            return Ok(drivers);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Driver>> GetDriversDetils(int id)
+        {
+            var driver = await _context.Drives.FirstOrDefaultAsync(d => d.Id == id);
+
+            if (driver == null)
+                return NotFound();
+
+            return Ok(driver);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDriver(Driver driver)
+        {
+            _context.Drives.Add(driver);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetDriversDetils), driver, driver.Id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDriver(Driver driver, int id)
+        {
+            var driverExist = await _context.Drives.FirstOrDefaultAsync(d => d.Id == id);
+
+            if (driverExist == null)
+                return NotFound();
+
+            driverExist.Name = driver.Name;
+            driverExist.RacingNb = driver.RacingNb;
+            driverExist.Team = driver.Team;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDriver(int id)
+        {
+            var driverExist = await _context.Drives.FirstOrDefaultAsync(d => d.Id == id);
+
+            if (driverExist == null)
+                return NotFound();
+
+            _context.Remove(driverExist.Id);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
