c57a1a914fd0ec6e87d0c8d318bd7ffd79dbc4b5
user <user@WINDOWS-PF2L7HE>
2025-02-05 02:34:23 +0500

    Add project files.

---
+++ FormulaOneV2/Server/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FormulaOneV2.Shared.Models;
+
+namespace FormulaOneV2.Server.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Driver> Drives { get; set; }
+    }
+}
---
+++ FormulaOneV2/Shared/Models/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOneV2.Shared.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RacingNb { get; set; } = string.Empty;
+        public string Team { get; set; } = string.Empty;
+    }
+}
